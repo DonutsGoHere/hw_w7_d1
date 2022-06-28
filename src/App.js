ee4ffdc6b5818aa795fb0f256c3350b89b57ed11
Calum Cook <calumcook@Calums-Air.broadband>
2022-06-28 01:19:56 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -16,9 +16,7 @@ function App() {
       <li key={index} className={task.highPriority ? "high-priority":"low-priority"}>
         <span>{task.name}</span>
 
-        {task.highPriority ? <span className="high-priority">High Priority!</span> : <label onClick={() => changePriority(index)}>low priority</label>}
-
-        <span>{task.highPriority}</span>
+        {task.highPriority ? <span className="high-priority">High Priority!</span> : <label>low priority</label>}
       </li>
     )
   })
@@ -52,8 +50,8 @@ function App() {
         <div><label>Add new task To-Do:</label></div>
         <div><input id="new_task" type="text" value={newTask} onChange={handleTaskInput}/></div>
         <div><input type="submit" value="Add new task"/></div>
-        <div><label>high</label><input type="radio" value="high-priority" checked={tasks.highPriority === true} onChange={changePriority}/></div>
-        <div><label>low</label><input type="radio" value="low-priority" checked={tasks.highPriority === false} onChange={changePriority}/></div>
+        <div><label>high</label><input type="radio" value="high-priority" onChange={changePriority}/></div>
+        <div><label>low</label><input type="radio" value="low-priority" onChange={changePriority}/></div>
       </form></div>
     </div>
   );
